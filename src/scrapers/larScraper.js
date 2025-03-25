0899f926295026c90b6dca5a069107e5269ab20b
RubensGJ <rubinho7.rr@gmail.com>
2025-03-25 11:08:41 -0400

    🐛  fix(api): corrigir erro no parser de dados

--- src/scrapers/larScraper.js
+++ src/scrapers/larScraper.js
@@ -5,7 +5,6 @@ async function scrapeLarAgro() {
     const url = "https://www.lar.ind.br/lar-agro/agricola/#cotacao";
     console.log("Iniciando navegador Puppeteer");
     const browser = await puppeteer.launch({
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
